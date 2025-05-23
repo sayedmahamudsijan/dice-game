d3fbb79e83f15ebcc255bef76f2df688aa82aee5
sayedmahamudsijan <112859381+sayedmahamudsijan@users.noreply.github.com>
2025-05-23 14:50:43 +0600

    Update fair-dice-roll.js

--- fair-dice-roll.js
+++ fair-dice-roll.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const CryptoUtils = require('./crypto-tools');
 const ConsoleUI = require('./game-console');
 
@@ -8,7 +9,7 @@ class FairRandomGenerator {
   }
   async generate(label) {
     const key = CryptoUtils.generateKey();
-    const compNum = CryptoUtils.generateRandomInt(this.rangeMax);
+    const compNum = crypto.randomInt(0, this.rangeMax + 1);
     const hmac = CryptoUtils.computeHMAC(key, compNum);
     console.log(`\n${label}\nHMAC=${hmac}\nPick a number (0-${this.rangeMax}):`);
     const userNum = await ConsoleUI.prompt('Your turn', Array(this.rangeMax + 1).fill().map((_, i) => i), this.dice);
@@ -17,5 +18,4 @@ class FairRandomGenerator {
     return result;
   }
 }
-
 module.exports = FairRandomGenerator;
